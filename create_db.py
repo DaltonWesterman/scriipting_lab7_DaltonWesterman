1b90e271dfedb0df9ada7b8e1d9405ec951a272d
Dalton Westerman <133789471+DaltonWesterman@users.noreply.github.com>
2023-07-12 20:29:32 -0400

    finished script for lab 7

---
+++ create_db.py
@@ -0,0 +1,116 @@
+"""
+Description:
+ Creates the people table in the Social Network database
+ and populates it with 200 fake people.
+
+Usage:
+ python create_db.py
+"""
+import os
+import sqlite3
+from datetime import datetime
+from faker import Faker
+
+# Determine the path of the database
+script_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(script_dir, 'social_network.db')
+
+def main():
+    create_people_table()
+    populate_people_table()
+
+def create_people_table():
+    """Creates the people table in the database"""
+    #Create function body
+    # Hint: See example code in lab instructions entitled "Creating a Table"
+    con = sqlite3.connect('social_network.db')
+    cur = con.cursor()
+
+    create_ppl_tbl_query = """
+        CREATE TABLE IF NOT EXISTS people
+        (
+        id          INTEGER PRIMARY KEY,
+        name        TEXT NOT NULL,
+        email       TEXT NOT NULL,
+        address     TEXT NOT NULL,
+        city        TEXT NOT NULL,
+        province    TEXT NOT NULL,
+        bio         TEXT,
+        age         INTEGER,
+        created_at  DATETIME NOT NULL,
+        updated_at  DATETIME NOT NULL
+        );
+    """
+    #Executes the query to create the table:
+    cur.execute(create_ppl_tbl_query)
+
+    #Saves the table and any changes made to it:
+    con.comit()
+
+    #close the database, any unsaved changes will be lost:
+    con.close
+
+    return
+
+def populate_people_table():
+    """Populates the people table with 200 fake people"""
+    #Create function body
+    con = sqlite3.connect('social_network.db')
+    cur = con.cursor()
+
+    add_person_query = """
+        INSERT INTO people
+        (
+            name,
+            email,
+            address,
+            city,
+            province,
+            bio,
+            age,
+            created_at,
+            updated_at
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+    """
+    new_person = ('Bob Loblaw',
+                  'bob.loblaw@whatever.net',
+                  '123 Fake St.',
+                  'Fakersville',
+                  'Fake Edward Island',
+                  'Enjoys making funny sounds when talking.',
+                  46,
+                  datetime.now(),
+                  datetime.now())
+    cur.execute(add_person_query, new_person)
+
+    con.commit()
+    con.close()
+
+    #Using Faker
+    
+    fake = Faker()
+    fake = Faker("en_CA")
+
+    for _ in range(200):
+
+        fake_person =   ( 
+                        fake.name(),
+                        fake.email(),
+                        fake.address(),
+                        fake.city(),
+                        fake.province(),
+                        fake.bio(),
+                        fake.age(min = 1, max = 100),
+                        datetime.now(),
+                        datetime.now())
+
+    cur.execute(add_person_query, fake_person)
+    con.commit()
+    con.close()
+    # Hint: See example code in lab instructions entitled "Inserting Data into a Table"
+    # Hint: See example code in lab instructions entitled "Working with Faker"
+    return
+
+if __name__ == '__main__':
+   main()
